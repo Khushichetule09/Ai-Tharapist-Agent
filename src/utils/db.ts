bd18fbe016e10d39005a317a149dc8d071f119f0
khushichetule09 <khushichetule2@gmail.com>
2025-09-27 17:52:49 +0530

    Update files

--- src/utils/db.ts
+++ src/utils/db.ts
@@ -3,8 +3,7 @@ import { logger } from "./logger";
 
 const MONGODB_URI =
   process.env.MONGODB_URI ||
-  "mongodb+srv://blocklinklabs:rKjw3YwImNAzgMzU@cluster0.znbgmwt.mongodb.net/ai-therapy";
-
+  
 export const connectDB = async () => {
   try {
     await mongoose.connect(MONGODB_URI);
